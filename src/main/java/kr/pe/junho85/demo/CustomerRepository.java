07eb50bb7bee197caeb8606623dac6a86ed3ca94
june.kim <junho85@gmail.com>
2020-07-15 13:51:45 +0900

    first commit

---
+++ src/main/java/kr/pe/junho85/demo/CustomerRepository.java
@@ -0,0 +1,10 @@
+package kr.pe.junho85.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+    Customer findById(long id);
+}
